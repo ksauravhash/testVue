e0dd28d804a1fe96e4114cc3eca44d05a481e307
Saurav Kumar <ksauravhash@gmail.com>
2025-05-17 09:51:25 +0530

    key management

---
+++ src/keys.ts
@@ -0,0 +1,8 @@
+import type { InjectionKey } from "vue";
+
+export type CartType = Record<string, number>;
+export type CartFunctionType = (num: number) => void;
+
+export const CART_KEY = Symbol() as InjectionKey<CartType>;
+export const ADD_TO_CART_KEY = Symbol() as InjectionKey<CartFunctionType>;
+export const REMOVE_FROM_CART_KEY = Symbol() as InjectionKey<CartFunctionType>;
